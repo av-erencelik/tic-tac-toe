fa130eec68823bdae94e6e697c63e7b696fd2e31
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-09-15 19:25:41 +0300

    playround function and some minor bugs

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,35 @@
-const gameBoard = (() => {
+const gameBoard = () => {
     let board = []
     let squares = document.querySelectorAll("#square")
     squares.forEach((e) => board.push(e))
-    console.log(board)
+    return board
+}
+const game = (player1,player2,board) => {
+    let squares = document.querySelectorAll("#square")
+    let xTurn = true
     squares.forEach(function(elem) {
-        elem.addEventListener("click", () => add(board,elem.getAttribute("data-index")))
+        elem.addEventListener("click", () => {
+            xTurn = playRound(board,elem.getAttribute("data-index"),xTurn,player1,player2)
+        })
     })
-})()
+    console.log(xTurn)
 
-function add(board, i) {
-    console.log(i)
-    board[i].innerHTML = "X"
+}
+function changeText(turn) {
+    let text = document.getElementById("text")
+    if(turn) {
+        text.textContent = "Player1's Turn"
+    }
+}
+
+function playRound(board, i,xTurn,player1,player2) {
+    if(xTurn) {
+        board[i].textContent = player1.sign
+        return false
+    }else {
+        board[i].textContent = player2.sign
+        return true
+    }
 }
 const createPlayer = (name,sign) => {
     return {name,sign}
@@ -22,11 +41,11 @@ const start = (() => {
 function signChoice() {
     document.getElementById("start").style.display = "none"
     let xSign = document.getElementById("x")
-    let ySign = document.getElementById("y")
+    let ySign = document.getElementById("o")
     xSign.style.display = "inline-block"
     ySign.style.display = "inline-block"
     xSign.addEventListener("click", () => handlePlayer("X",xSign,ySign))
-    ySign.addEventListener("click", () => handlePlayer("Y",xSign,ySign))
+    ySign.addEventListener("click", () => handlePlayer("O",xSign,ySign))
 }
 function handlePlayer(sign,xSign,ySign) {
     xSign.style.display = "none"
@@ -37,10 +56,13 @@ function handlePlayer(sign,xSign,ySign) {
     let player2
     if(sign === "X") {
         player1 = createPlayer("player1", "X")
-        player2 = createPlayer("player2", "Y")
+        player2 = createPlayer("player2", "O")
     }else {
-        player2 = createPlayer("player2", "Y")
+        player2 = createPlayer("player2", "O")
         player1 = createPlayer("player1", "X")
     }
     console.log(player1,player2)
+    let board = gameBoard()
+    game(player1,player2,board)
+    
 }
